d1893c8887e6cb35c09d08ec87448a3b9d67a33d
Expurgator <timkayhou@gmail.com>
2022-04-03 15:55:21 +0900

    🌈 style: lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import i18n from './i18n'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import i18n from './i18n'
+import router from './router'
+import store from './store'
 
 loadFonts()
 
